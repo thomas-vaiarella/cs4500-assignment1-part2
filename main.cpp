66b962b4340401e1ae22a1e25fafa55fd844ae36
DNishanth <dnishanth99@hotmail.com>
2020-01-23 21:34:47 -0500

    Store fields in tmpArray

--- main.cpp
+++ main.cpp
@@ -3,8 +3,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <vector>
- 
- 
+#include <iostream>
+#include <string.h>
+#include <string>
+#include "column.h"
+
+bool isString(std::string) {
+    // if (isAlpha(tmpTable[c].at(r).at(i))) {
+    //     colType = STRING;
+    //     foundType = True;
+    //     break;
+    // }
+}
+
 int main(int argc, char** argv) {
     int flag; // stores current flag from getopt()
     const char* fileName = NULL; // empty string is default if -f flag is missing
@@ -49,93 +60,146 @@ int main(int argc, char** argv) {
     char* fileContents = new char[fileSize];
     fread(fileContents, 1, fileSize, file);
     fclose(file);
-    // fileContents[fileSize] = 0;
  
  
-    bool foundStartIndex = 0; // has the first index of possible word been seen?
-    // size_t startIndex = 0; // first index of possible word
-    // std::vector *table = vector[];
+    bool foundStartIndex = false; // has the first index of possible word been seen?
+    size_t startIndex = 0; // first index of possible word
  
     size_t maxFields = 0;
     size_t numFields = 0;
 
     for (size_t i = 0; i < fileSize; i++) {
-        if (!foundStartIndex && fileContents[i] == '<') {
+        if (fileContents[i] == '\n' || i == fileSize - 1) {
+            if (fileContents[i] == '>') {
+                numFields++;
+            }
+            if (numFields > maxFields) {
+                maxFields = numFields;
+            }
+            numFields = 0;
+            foundStartIndex = false;
+        }
+        else if (!foundStartIndex && fileContents[i] == '<') {
             foundStartIndex = true;
         }
         else if (foundStartIndex && fileContents[i] == '>') {
             foundStartIndex = false;
             numFields++;
         }
-        else if (fileContents[i] == '\n') {
-            if (numFields > maxFields) {
-                maxFields = numFields;
-            }
-            numFields = 0;
-        }
     }
-    printf("%zu\n", maxFields);
-    exit(0);
-}
+    
+    std::vector<std::string> tmpTable[maxFields];
+    size_t currCol = 0;
 
-//     // parse through file by character
-//     for (size_t i = 0; i < fileSize; i++) {
-//         int isAlpha = isalpha(fileContents[i]);
-        
-//         if (!foundStartIndex && isAlpha) {
-//             foundStartIndex = true;
-//             startIndex = i;
-//         }
-//         else if (foundStartIndex && (!isAlpha || i == fileSize - 1)) {
-//             // found a word
-//             size_t strLen = i - startIndex;
- 
-//             // increase capacity of char* for final word by 1
-//             if (i == fileSize - 1) {
-//                 strLen += 1;
-//                 // printf("%c\n", fileContents[i]);
-//                 // printf("%zu\n", startIndex);
-//                 // printf("%zu\n", strLen);
-//             }
+    // parse through file by character
+    for (size_t i = 0; i < fileSize; i++) {        
+        if (!foundStartIndex && fileContents[i] == '<') {
+            foundStartIndex = true;
+            startIndex = i + 1;
+        }
+        else if (foundStartIndex && fileContents[i] == '>') {
+            // found a field
+            size_t strLen = i - startIndex;
+            if (strLen == 0) {
+                tmpTable[currCol].push_back("");
+            }
+            else {
+                // create String from file contents array
+                char* newStr = new char[strLen + 1];
+                strncpy(newStr, fileContents+startIndex, strLen);
+                newStr[strLen + 1] = 0;
+                tmpTable[currCol].push_back(std::string(newStr));
+            }
+            foundStartIndex = false;
+            currCol++;
+        }
+        else if (fileContents[i] == '\n') {
+            while (currCol < maxFields) {
+                tmpTable[currCol].push_back("");
+                currCol++;
+            }
+            currCol = 0;
+        }
+
+
+            // // increase capacity of char* for final word by 1
+            // if (i == fileSize - 1) {
+            //     strLen += 1;
+            //     // printf("%c\n", fileContents[i]);
+            //     // printf("%zu\n", startIndex);
+            //     // printf("%zu\n", strLen);
+            // }
             
-//             // skip words < 4 letters
-//             if (strLen <= 4) {
-//                 foundStartIndex = false;
-//                 continue;
-//             }
+            // // skip words < 4 letters
+            // if (strLen <= 4) {
+            //     foundStartIndex = false;
+            //     continue;
+            // }
  
-//             // create String from file contents array
-//             char* newStr = new char[strLen + 1];
-//             strncpy(newStr, fileContents+startIndex, strLen);
-//             newStr[strLen + 1] = 0;
+            // // create String from file contents array
+            // char* newStr = new char[strLen + 1];
+            // strncpy(newStr, fileContents+startIndex, strLen);
+            // newStr[strLen + 1] = 0;
  
-//             // convert to lowercase
-//             for (size_t i = 0; i < strLen; i++) {
-//                 newStr[i] = tolower(newStr[i]);
-//             }
+            // // convert to lowercase
+            // for (size_t i = 0; i < strLen; i++) {
+            //     newStr[i] = tolower(newStr[i]);
+            // }
  
-//             // CountString stores String and Count
-//             CountString *str = new CountString(newStr);
-//             delete newStr;
+            // // CountString stores String and Count
+            // CountString *str = new CountString(newStr);
+            // delete newStr;
  
-//             size_t listIndex = list->index_of(str);
-//             if (listIndex > list->size()) {
-//                 // add to list if new String
-//                 list->sorted_add(str);
-//             }
-//             else {
-//                 // remove old String and add with higher count
-//                 size_t newCount = list->get(listIndex)->count + 1;
-//                 str->count = newCount;
-//                 list->remove(listIndex);
-//                 list->sorted_add(str);
-//             }
-//             // reset for next word start index
-//             foundStartIndex = false;
-//         }
-//     }
-//     list->printWithCounts();
-//     delete fileContents;
-//     delete list;
-//     return 0;
-// }
+            // size_t listIndex = list->index_of(str);
+            // if (listIndex > list->size()) {
+            //     // add to list if new String
+            //     list->sorted_add(str);
+            // }
+            // else {
+            //     // remove old String and add with higher count
+            //     size_t newCount = list->get(listIndex)->count + 1;
+            //     str->count = newCount;
+            //     list->remove(listIndex);
+            //     list->sorted_add(str);
+            // }
+            // // reset for next word start index
+            // foundStartIndex = false;
+    }
+
+    enum Type {BOOL, INTEGER, FLOAT, STRING}
+    Type colType = BOOL;
+    Column table[maxFields];
+    bool foundType = false;
+
+    for (size_t c = 0; c < maxFields; c++) {
+        if (foundType) {
+            createColumn(colType, pointerToColumn);
+            continue;
+        }
+        for (size_t r = 0; r < tmpTable[c].size(); r++) {
+            std::String* currStr = &tmpTable[c].at(r);
+            if (isCertainlyString(currStr) {
+                colType = STRING;
+                foundType = true;
+                break;
+            }
+            else if (possiblyFloat(currStr)) {
+                colType = FLOAT;
+            }
+            else if (possiblyInt(currStr)) {
+                colType = INTEGER;
+            }
+        }
+        createColumn(colType, pointerToColumn);
+        foundType = false;
+    }
+
+    // for (size_t i = 0; i < tmpTable[6].size(); i++) {
+    //     std::cout << tmpTable[6].at(i) << "\n";
+    // }
+    
+    // list->printWithCounts();
+    // delete fileContents;
+    // delete list;
+    return 0;
+}
